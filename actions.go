92796e39401336d61fd1612b83ad2b7e88f874c0
Anirban Gupta <agupta666@gmail.com>
2016-09-07 17:37:48 +0530

    added file action

--- actions.go
+++ actions.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bytes"
+	"io/ioutil"
+	"os"
 	"os/exec"
 	"strings"
 )
@@ -51,11 +53,40 @@ func (sa *ShellAction) Exec() ([]byte, error) {
 	return out.Bytes(), nil
 }
 
+// FileAction action represents actions which responds with the contents of a file
+type FileAction struct {
+	Path string
+}
+
+// NewFileAction creates a new FileAction
+func NewFileAction(p string) *FileAction {
+	return &FileAction{Path: p}
+}
+
+// Exec executes a file action
+func (fa *FileAction) Exec() ([]byte, error) {
+	r, err := os.Open(fa.Path)
+	if err != nil {
+		return make([]byte, 0), err
+	}
+	b, err := ioutil.ReadAll(r)
+
+	if err != nil {
+		return make([]byte, 0), err
+	}
+	return b, nil
+}
+
 func getAction(arg string) Action {
 	actionIdentifier := arg[0]
-	if actionIdentifier == '!' {
+
+	switch actionIdentifier {
+	case '!':
 		return NewShellAction(arg[1:])
+	case '@':
+		return NewFileAction(arg[1:])
+	default:
+		return NewStringAction(arg)
 	}
 
-	return NewStringAction(arg)
 }
